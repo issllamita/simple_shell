0eec26720f46024308b74cf58d22197383f596d3
leissini <layssini15@gmail.com>
2023-08-21 09:19:11 -0700

    1st commit

---
+++ system_calls.c
@@ -0,0 +1,119 @@
+#include "main.h"
+
+/**
+ * read_line - reads the input string.
+ * @edf: return value of getline function.
+ * Return: input string
+ */
+char *read_line(int *edf)
+{
+	char *read = NULL;
+	size_t bufsize = 0;
+
+	*edf = getline(&read, &bufsize, stdin);
+
+	return (read);
+}
+
+/**
+ * show_line - assigns the line var for get_line
+ * @lineptr: Buffer that store the input str
+ * @buffer: str that is been called to line
+ * @n: size of line
+ * @j: size of buffer
+ */
+void show_line(char **lineptr, size_t *n, char *buffer, size_t j)
+{
+
+	if (*lineptr == NULL)
+	{
+		if  (j > BUFSIZE)
+			*n = j;
+
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else if (*n < j)
+	{
+		if (j > BUFSIZE)
+			*n = j;
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+}
+/**
+ * get_line - Read inpt from stream
+ * @lineptr: buffer that stores the input
+ * @n: size of lineptr
+ * @stream: stream to read from
+ * Return: The number of bytes
+ */
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
+{
+	int i;
+	static ssize_t read;
+	ssize_t retval;
+	char *buffer;
+	char t = 'z';
+
+	if (read == 0)
+		fflush(stream);
+	else
+		return (-1);
+	read = 0;
+
+	buffer = malloc(sizeof(char) * BUFSIZE);
+	if (buffer == 0)
+		return (-1);
+	while (t != '\n')
+	{
+		i = read(STDIN_FILENO, &t, 1);
+		if (i == -1 || (i == 0 && read == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (i == 0 && read != 0)
+		{
+			read++;
+			break;
+		}
+		if (read >= BUFSIZE)
+			buffer = _realloc(buffer, read, read + 1);
+		buffer[read] = t;
+		read++;
+	}
+	buffer[read] = '\0';
+	show_line(lineptr, n, buffer, read);
+	retval = read;
+	if (i != 0)
+		read = 0;
+	return (retval);
+}
+
+/**
+ * exec_line - finds builtins and commands
+ * @datash: data relevant (args)
+ * Return: 1 on success.
+ */
+int exec_line(data_sh *datash)
+{
+	int (*builtin)(data_sh *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
