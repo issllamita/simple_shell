84926a36c605ff3e6e298064daf6add6a908a694
leissini <layssini15@gmail.com>
2023-08-22 07:49:44 -0700

    update

--- help_func2.c
+++ help_func2.c
@@ -6,13 +6,13 @@
  */
 void help(void)
 {
-	char *help = "help: help [-dms] [pattern ...]\n";
+	char *hlp = "help: help [-dms] [pattern ...]\n";
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tDisplay information about builtin commands.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Displays brief summaries of builtin commands.\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	hlp = "\tDisplay information about builtin commands.\n ";
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	hlp = "Displays brief summaries of builtin commands.\n";
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 }
 /**
  * help_alias - Help information for the builtin alias.
@@ -20,11 +20,11 @@ void help(void)
  */
 void help_alias(void)
 {
-	char *help = "alias: alias [-p] [name[=value]...]\n";
+	char *hlp = "alias: alias [-p] [name[=value]...]\n";
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tDefine or display aliases.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	hlp = "\tDefine or display aliases.\n ";
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 }
 /**
  * help_cd - Help information for the builtin alias.
@@ -32,9 +32,9 @@ void help_alias(void)
  */
 void help_cd(void)
 {
-	char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
+	char *hlp = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tChange the shell working directory.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
+	hlp = "\tChange the shell working directory.\n ";
+	write(STDOUT_FILENO, hlp, _strlen(hlp));
 }
