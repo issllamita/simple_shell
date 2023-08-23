3cc62cfd994541a27ddd112887f1001a03866d5d
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-08-23 11:45:16 -0700

    update

--- str_func1.c
+++ str_func1.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcpy - copy a string pointed by source
- * @dest: destination value
- * @src: sourcei value
+ * @dest: destination string
+ * @src: source string
  * Return: the pointer to destination
  */
 char *_strcpy(char *dest, char *src)
@@ -14,7 +14,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	dest[i++] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
 
